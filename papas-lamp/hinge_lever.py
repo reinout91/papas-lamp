7f15b23d4545168a8d62dbe005eb959c5f7ae1c6
Reinout Holtrup <reinout91@gmail.com>
2024-10-26 02:07:28 +0200

    fixes

--- papas-lamp/hinge_lever.py
+++ papas-lamp/hinge_lever.py
@@ -16,19 +16,21 @@ W1 = 6 * MM
 H = 6 * MM
 H2 = 3.9 * MM
 H3 = 4 * MM
+pin_radius = 2
+hole_radius = 1.9
 
 with BuildPart() as first:
     with Locations((-10, 0, 0)):
         bla = Box(6, 50, H, align=(Align.MAX, Align.CENTER, Align.CENTER))
         Box(14, 6.1, H2, align=(Align.MIN, Align.CENTER, Align.CENTER))
     with Locations(bla.faces().filter_by(Axis.Y)[-1].center_location):
-        bla = Box(H2, 6, 10, align=(Align.CENTER, Align.CENTER, Align.MIN))
+        bla = Box(H2, 6, 8, align=(Align.CENTER, Align.CENTER, Align.MIN))
     with Locations(
         bla.faces().filter_by(Axis.Y)[-1].center_location.position + (0, -1, 0)
     ):
-        Cylinder(2.5, H, align=(Align.CENTER, Align.MAX, Align.CENTER))
+        Cylinder(pin_radius, H, align=(Align.CENTER, Align.MAX, Align.CENTER))
 
-    Cylinder(2, 999, mode=Mode.SUBTRACT)
+    Cylinder(hole_radius, 999, mode=Mode.SUBTRACT)
     max_fillet = first.part.max_fillet(
         first.edges().filter_by(Axis.Z), tolerance=0.2, max_iterations=20
     )
@@ -46,13 +48,13 @@ with BuildPart() as second:
             align=(Align.MAX, Align.CENTER, Align.CENTER),
             mode=Mode.SUBTRACT,
         )
-    cyl = Cylinder(1.9, H, align=(Align.MIN, Align.CENTER, Align.CENTER))
+    cyl = Cylinder(pin_radius, H, align=(Align.MIN, Align.CENTER, Align.CENTER))
     with Locations(bla.faces().filter_by(Axis.X)[-1].center_location.position):
         bla = Box(7, W1, H2, align=(Align.MIN, Align.CENTER, Align.CENTER))
     with Locations(
         bla.faces().filter_by(Axis.X)[-1].center_location.position + (-1, 0, 0)
     ):
-        Cylinder(2, H, align=(Align.MAX, Align.CENTER, Align.CENTER))
+        Cylinder(pin_radius, H, align=(Align.MAX, Align.CENTER, Align.CENTER))
     max_fillet = second.part.max_fillet(
         second.edges().filter_by(Axis.Z), tolerance=2, max_iterations=20
     )
