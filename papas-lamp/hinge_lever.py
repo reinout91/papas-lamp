2c03391c1ff3ef02a49669bd3277feb11285a17d
Reinout Holtrup <reinout91@gmail.com>
2024-10-28 21:17:24 +0100

    improve hinge (#1)
    * new approach
    
    * fixes
    
    * use hole instead of cylinder
    
    * ideal vertical gap
    
    * improve part names

--- papas-lamp/hinge_lever.py
+++ papas-lamp/hinge_lever.py
@@ -6,85 +6,77 @@ from build123d import *
 from build123d.topology import tuplify
 from ocp_vscode import *
 
+
 set_port(3939)
 
 print(build123d.__version__)
 
+# specify parameters on base of the hinges
+ideal_layer_height = 0.2 * MM
+ideal_vert_gap = 2 * ideal_layer_height
 
-#specify parameters on base of the hinges
-H=6*MM #height of the hinge lever. has to be larger than rad_H.
-pin_L=6*MM #length of the pin of all levers. (handy for printing to keep the same as H)
-B=3*MM #thickness of the hinge lever with 0.2 margin.
-rad_H=1.8*MM #radius of the hinge pin with 0.1 margin (times 2 for diameter).
-tol=0.3*MM #a tollerance between rotating parts. has to be smaller than all other parameters.
-tol_arm=1.5*MM #tollerance of the arms to the rotation point.
-arm2base_L=60*MM #length of the first arm
-arm2outer_ring_L=20*MM #length of the second arm
+W1 = 6 * MM
+H = 6 * MM
+H3 = 3 * MM
+H2 = H3 - ideal_vert_gap
+pin_radius = 1.8 * MM
+hole_radius = 2 * MM
 
-with BuildPart () as arm2base:
-    Box(H,arm2base_L,pin_L)
+macc = (Align.MAX, Align.CENTER, Align.CENTER)
+micc = (Align.MIN, Align.CENTER, Align.CENTER)
 
-    #making snap fingers at the end of the arm.
-    with Locations ((0,arm2base_L/2,-pin_L/2),(0,arm2base_L/2,pin_L/2)):
-        Box(H,2*rad_H+tol_arm,pin_L-B,
-            align=(Align.CENTER,Align.MAX,Align.CENTER),
-            mode=Mode.SUBTRACT
+with BuildPart() as t_hinge:
+    with Locations((-10, 0, 0)):
+        bla = Box(6, 50, H, align = macc)
+        Box(14, 6, H2, align = micc)
+    with Locations(bla.faces().filter_by(Axis.Y)[-1].center_location):
+        bla = Box(H2, 6, 8, align=(Align.CENTER, Align.CENTER, Align.MIN))
+    with Locations(
+        bla.faces().filter_by(Axis.Y)[-1].center_location.position + (0, -1, 0)
+    ):
+        Cylinder(pin_radius, H, align=(Align.CENTER, Align.MAX, Align.CENTER))
+
+    Hole(hole_radius)
+    max_fillet = t_hinge.part.max_fillet(
+        t_hinge.edges().filter_by(Axis.Z), tolerance=0.2, max_iterations=20
+    )
+    fillet(objects=t_hinge.edges().filter_by(Axis.Z), radius=max_fillet)
+
+with BuildPart() as shackle:
+    with Locations((-2, 0, 0)):
+        bla = Box(10, W1, H, align = micc)
+    with Locations(bla.faces().filter_by(Axis.X)[-1].center_location.position):
+        bla = Box(10, W1, H, align = micc)
+        Box(
+            80,
+            30,
+            H3,
+            align = macc,
+            mode = Mode.SUBTRACT,
         )
-    with Locations((0,arm2base_L/2-rad_H-2*tol,0)):
-        Cylinder(rad_H, pin_L,
-                 )
+    cyl = Cylinder(pin_radius, H, align = micc)
+    with Locations(bla.faces().filter_by(Axis.X)[-1].center_location.position):
+        bla = Box(7, W1, H2, align = micc)
+    with Locations(
+        bla.faces().filter_by(Axis.X)[-1].center_location.position + (-1, 0, 0)
+    ):
+        Cylinder(pin_radius, H, align = macc)
+    max_fillet = shackle.part.max_fillet(
+        shackle.edges().filter_by(Axis.Z), tolerance = 2, max_iterations = 20
+    )
+    fillet(objects=shackle.edges().filter_by(Axis.Z), radius=max_fillet)
 
+j1 = RevoluteJoint(label="t_hinge_hole", to_part=t_hinge.part)
+j2 = RigidJoint(
+    label="shackle_pin", to_part=shackle.part, joint_location=Location(cyl.center())
+)
+j2.connect_to(j1, angle = 0)
 
-    #join the other arm with an inner pin hinge
-    with Locations ((H/2,0,0)):
-        Box(rad_H*2+tol_arm+2*tol,H,B-tol/2,
-            align=(Align.MIN, Align.CENTER, Align.CENTER),
-            )
-    with Locations((H/2+tol_arm,0,0)):
-        Cylinder(rad_H,
-                 pin_L,
-                 rotation=(0,0,0),
-                 align=(Align.MIN, Align.CENTER, Align.CENTER),
-                 mode=Mode.SUBTRACT
-                 )
+del micc, macc, bla, cyl, j1, j2
 
-    fillet(objects=arm2base.edges().filter_by(Axis.Z),radius=rad_H+tol)
-
-
-
-with BuildPart () as arm2outer_ring:
-    with Locations ((H/2+2*rad_H+2*tol_arm,0,0)):
-        Box(arm2outer_ring_L,H,pin_L,
-            align=(Align.MIN, Align.CENTER, Align.CENTER)
-            )
-
-    #making snap fingers at the end of the arm
-    with Locations ((arm2outer_ring_L+H/2+2*rad_H+2*tol_arm,0,-pin_L/2),(arm2outer_ring_L+H/2+2*rad_H+2*tol_arm,0,pin_L/2)):
-        Box(2*rad_H+tol_arm,H,pin_L-B,
-            align=(Align.MAX,Align.CENTER,Align.CENTER),
-            mode=Mode.SUBTRACT
-        )
-    with Locations((arm2outer_ring_L+H/2+2*rad_H+2*tol_arm-rad_H-2*tol,0,0)):
-        Cylinder(rad_H, pin_L,
-                 )
-
-
-    #join the other arm with an inner pin hinge
-    with Locations ((H/2+tol_arm,0,-pin_L/2+(pin_L-B-tol)/4),(H/2+tol_arm,0,pin_L/2-(pin_L-B-tol)/4)):
-         Box(2*rad_H+tol_arm,H,(pin_L/2-(B+tol)/2),
-             align=(Align.MIN,Align.CENTER,Align.CENTER)
-             )
-    with Locations ((H/2+tol_arm+tol,0,0)):
-        Cylinder(rad_H-tol,
-                 pin_L,
-                 rotation=(0,0,0),
-                 align=(Align.MIN, Align.CENTER, Align.CENTER),
-                 )
-    fillet(objects=arm2outer_ring.edges().filter_by(Axis.Z), radius=rad_H+tol)
-
-
-comp_hinge_arms=Compound((arm2base.part,arm2outer_ring.part))
+shackle.part.color = "red"
 
 show_all(reset_camera=Camera.KEEP)
 
-comp_hinge_arms.export_step("hinge_lever.step")
+comp_hinge_arms = Compound((t_hinge.part, shackle.part))
+export_step(comp_hinge_arms, "hinge_lever.step")
