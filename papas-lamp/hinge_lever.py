baae7664840fe0ab9327813365214902b161aefa
Reinout Holtrup <reinout91@gmail.com>
2024-10-28 14:56:50 +0100

    use hole instead of cylinder

--- papas-lamp/hinge_lever.py
+++ papas-lamp/hinge_lever.py
@@ -16,13 +16,16 @@ W1 = 6 * MM
 H = 6 * MM
 H2 = 3.9 * MM
 H3 = 4 * MM
-pin_radius = 2
-hole_radius = 1.9
+pin_radius = 2 * MM
+hole_radius = 1.9 * MM
+
+macc = (Align.MAX, Align.CENTER, Align.CENTER)
+micc = (Align.MIN, Align.CENTER, Align.CENTER)
 
 with BuildPart() as first:
     with Locations((-10, 0, 0)):
-        bla = Box(6, 50, H, align=(Align.MAX, Align.CENTER, Align.CENTER))
-        Box(14, 6.1, H2, align=(Align.MIN, Align.CENTER, Align.CENTER))
+        bla = Box(6, 50, H, align = macc)
+        Box(14, 6.1, H2, align = micc)
     with Locations(bla.faces().filter_by(Axis.Y)[-1].center_location):
         bla = Box(H2, 6, 8, align=(Align.CENTER, Align.CENTER, Align.MIN))
     with Locations(
@@ -30,7 +33,7 @@ with BuildPart() as first:
     ):
         Cylinder(pin_radius, H, align=(Align.CENTER, Align.MAX, Align.CENTER))
 
-    Cylinder(hole_radius, 999, mode=Mode.SUBTRACT)
+    Hole(hole_radius)
     max_fillet = first.part.max_fillet(
         first.edges().filter_by(Axis.Z), tolerance=0.2, max_iterations=20
     )
@@ -38,25 +41,25 @@ with BuildPart() as first:
 
 with BuildPart() as second:
     with Locations((-2, 0, 0)):
-        bla = Box(10, W1, H, align=(Align.MIN, Align.CENTER, Align.CENTER))
+        bla = Box(10, W1, H, align = micc)
     with Locations(bla.faces().filter_by(Axis.X)[-1].center_location.position):
-        bla = Box(10, W1, H, align=(Align.MIN, Align.CENTER, Align.CENTER))
+        bla = Box(10, W1, H, align = micc)
         Box(
             80,
             30,
             H3,
-            align=(Align.MAX, Align.CENTER, Align.CENTER),
-            mode=Mode.SUBTRACT,
+            align = macc,
+            mode = Mode.SUBTRACT,
         )
-    cyl = Cylinder(pin_radius, H, align=(Align.MIN, Align.CENTER, Align.CENTER))
+    cyl = Cylinder(pin_radius, H, align = micc)
     with Locations(bla.faces().filter_by(Axis.X)[-1].center_location.position):
-        bla = Box(7, W1, H2, align=(Align.MIN, Align.CENTER, Align.CENTER))
+        bla = Box(7, W1, H2, align = micc)
     with Locations(
         bla.faces().filter_by(Axis.X)[-1].center_location.position + (-1, 0, 0)
     ):
-        Cylinder(pin_radius, H, align=(Align.MAX, Align.CENTER, Align.CENTER))
+        Cylinder(pin_radius, H, align = macc)
     max_fillet = second.part.max_fillet(
-        second.edges().filter_by(Axis.Z), tolerance=2, max_iterations=20
+        second.edges().filter_by(Axis.Z), tolerance = 2, max_iterations = 20
     )
     fillet(objects=second.edges().filter_by(Axis.Z), radius=max_fillet)
 
@@ -64,12 +67,10 @@ j1 = RevoluteJoint(label="first_hole", to_part=first.part)
 j2 = RigidJoint(
     label="second_pin", to_part=second.part, joint_location=Location(cyl.center())
 )
-j2.connect_to(j1, angle=20)
+j2.connect_to(j1, angle = 0)
 
-del bla
-del cyl
-del j1
-del j2
+del micc, macc, bla, cyl, j1, j2
+
 second.part.color = "red"
 
 
