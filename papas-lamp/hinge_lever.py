f0b4a92d7e03b300a693b3344f2c307d44215861
Reinout Holtrup <reinout91@gmail.com>
2024-11-10 20:42:26 +0100

    improved angles of hinge

--- papas-lamp/hinge_lever.py
+++ papas-lamp/hinge_lever.py
@@ -5,7 +5,7 @@ import build123d
 from build123d import *
 from build123d.topology import tuplify
 from ocp_vscode import *
-from build123d_ease import align
+from build123d_ease import align, back_face_of
 
 set_port(3939)
 
@@ -24,13 +24,14 @@ hole_radius = 2 * MM
 
 
 with BuildPart() as t_hinge:
-    with Locations((-10, 0, 0)):
-        bla = Box(6, 50, H, align = align.RIGHT)
-        Box(14, 6, H2, align = align.LEFT)
-    with Locations(bla.faces().filter_by(Axis.Y)[-1].center_location):
-        bla = Box(H2, 6, 8, align=align.BOTTOM)
+    with Locations((-W1, -5, 0)):
+        bla = Box(W1, 50, H, align = align.RIGHT)
+    with Locations((-W1, 0, 0)):
+        Box(10, W1, H2, align = align.LEFT)
+    with Locations(back_face_of(bla).center_location):
+        bla = Box(H2, W1, 8, align=align.BOTTOM)
     with Locations(
-        bla.faces().filter_by(Axis.Y)[-1].center_location.position + (0, -1, 0)
+       back_face_of(bla).center_location.position + (0, -1, 0)
     ):
         Cylinder(pin_radius, H, align=align.BACK)
 
@@ -44,17 +45,11 @@ with BuildPart() as shackle:
     with Locations((-2, 0, 0)):
         bla = Box(10, W1, H, align = align.LEFT)
     with Locations(bla.faces().filter_by(Axis.X)[-1].center_location.position):
-        bla = Box(10, W1, H, align = align.LEFT)
-        Box(
-            80,
-            30,
-            H3,
-            align = align.RIGHT,
-            mode = Mode.SUBTRACT,
-        )
+        bla = Box(14, W1, H, align = align.LEFT)
+        Box(80,30,H3,align = align.RIGHT, mode = Mode.SUBTRACT)
     cyl = Cylinder(pin_radius, H, align = align.LEFT)
     with Locations(bla.faces().filter_by(Axis.X)[-1].center_location.position):
-        bla = Box(7, W1, H2, align = align.LEFT)
+        bla = Box(8, W1, H2, align = align.LEFT)
     with Locations(
         bla.faces().filter_by(Axis.X)[-1].center_location.position + (-1, 0, 0)
     ):
@@ -68,7 +63,7 @@ j1 = RevoluteJoint(label="t_hinge_hole", to_part=t_hinge.part)
 j2 = RigidJoint(
     label="shackle_pin", to_part=shackle.part, joint_location=Location(cyl.center())
 )
-j2.connect_to(j1, angle = 0)
+j1.connect_to(j2, angle = 90)
 
 del align.LEFT, align.RIGHT, bla, cyl, j1, j2
 
