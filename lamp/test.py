5ee0768e3fc632fdad0d159664d6380331cbd3bf
Reinout Holtrup <reinout91@gmail.com>
2025-01-06 02:11:11 +0100

    wip poetry -> uv

--- lamp/test.py
+++
@@ -1,6 +0,0 @@
-import build123d as bd
-from ocp_vscode import Camera, show_all
-
-p = bd.Part(None) + bd.Box(length=30, width=60, height=30)
-
-show_all(reset_camera=Camera.KEEP)
--- papas-lamp/__init__.py
+++ papas-lamp/__init__.py
@@ -1,1 +1,2 @@
-__version__ = '0.1.0'
+def hello() -> None:
+    print("Hello from hello-world!")
