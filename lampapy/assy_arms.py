211eea9899b34ad08f5485a5a577b5ad76d051f1
Reinout Holtrup <reinout91@gmail.com>
2025-01-14 00:10:17 +0100

    add polar_locations_from_rectangular_locations

--- lampapy/assy_arms.py
+++ lampapy/assy_arms.py
@@ -17,10 +17,14 @@ from build123d import (
     Box,
     BuildPart,
     Align,
+    PositionMode,
+    FrameMethod,
+    Line,
+    Plane,
 )
 from hexacon_pin import hexacon_pin
 from ocp_vscode import Camera, set_port, show_all
-from utils import axis_from_three_points
+from utils import axis_from_three_points, polar_locations_from_rectangular_locations
 
 # AttributeError: 'list' object has no attribute 'values'
 # centre = 0.5 * sum(surface.position_at(i, 0.5) for i in [0, 0.5])
@@ -36,11 +40,10 @@ grouped_shapes = ShapeList(
 ).group_by(lambda f: (f.direction.X, f.direction.Y, f.direction.Z))
 pts = [Vector(0.5 * (pair[0].position + pair[1].position)) for pair in grouped_shapes]
 
-print(pts)
+
 central_axis = axis_from_three_points(pts)
-with BuildPart(central_axis.to_plane()) as p:
-    with PolarLocations(radius=pts[0].length-0.5, count=len(pts), start_angle=180):
-        Box(1, 1, 1)
+
+a = polar_locations_from_rectangular_locations(pts, central_axis)
 
 
 if __name__ == "__main__":
