c1fe1d5232e0929f3d003ea0c37f5e19658408b7
Reinout Holtrup <reinout91@gmail.com>
2025-01-25 15:12:42 +0100

    add rotation to icosahedron

--- lampapy/icosahedron_joint.py
+++ lampapy/icosahedron_joint.py
@@ -1,7 +1,17 @@
-from math import sqrt
+from math import sqrt, asin, pi
 
-from build123d import Align, Box, Cylinder, Line, Location, Part, Vector, export_step
+from build123d import (
+    Align,
+    Box,
+    Cylinder,
+    Line,
+    Location,
+    Part,
+    Vector,
+    export_step,
+)
 from ocp_vscode import Camera, set_port, show_all
+
 
 # Constants
 PHI = (1 + sqrt(5)) / 2
@@ -12,7 +22,9 @@ OFFSET = 3.6
 OFFSET_LOCATION = Location((0, 0, OFFSET))
 BOTTOM_ALIGN = (Align.CENTER, Align.CENTER, Align.MIN)
 FLOOR_OFFSET = Location((0, 0, 5.9))
+angle_xob = asin(1 / sqrt(1.618**2 + 1)) * (180 / pi)
 
+print(angle_xob)
 # Icosahedron vertices
 icosahedron_vertices = (
     [Vector(0, i, j * PHI) for i in [-1, 1] for j in [-1, 1]]
@@ -36,7 +48,7 @@ def create_hollow_cylinder(line):
     )
 
 
-icosahedron_joint = Part(None)
+icosahedron_joint: Part = Part()
 
 # Add hollow cylinders along primary axes
 for axis in axes:
@@ -47,7 +59,7 @@ icosahedron_joint += additional_axis.to_plane() * Cylinder(1.3, 3.83)
 icosahedron_joint -= (
     additional_axis.to_plane() * FLOOR_OFFSET * Box(20, 20, 4, align=BOTTOM_ALIGN)
 )
-
+icosahedron_joint.locate(Location((0, 0, 0), (angle_xob, 0, 0)))
 
 # Visualization
 if __name__ == "__main__":
