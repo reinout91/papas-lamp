45bba7f3b1e3165398708cedb4a5cb0943a568cb
Reinout Holtrup <reinout91@gmail.com>
2025-02-19 00:33:54 +0100

    First working model, very ugly code

--- lampapy/insert.py
+++ lampapy/insert.py
@@ -1,6 +1,9 @@
 from build123d import (
+    Face,
     Line,
     Polyline,
+    Shell,
+    Solid,
     Spline,
     Vector,
     Wire,
@@ -38,8 +41,8 @@ top = [
     Vector(14, 0, -50),
 ]
 
-wire1 = Wire(Polyline([b0, m0, m1, b1, b0]))
-wire2 = Wire(Polyline([b6, m6, m2, b2, b6]))
+wire1 = Wire(Polyline(b0, m0, m1, b1, b0))
+
 wire3 = Wire(
     [
         Spline([b3, d3, m3, o[0]]),
@@ -48,38 +51,69 @@ wire3 = Wire(
         Line(b5, b3),
     ]
 )
-p1 = Polyline(b0, b6, b5)
-p2 = Polyline(b1, b2, b3)
-p3 = Polyline(m0, m6, o[1])
-p4 = Polyline(m1, m2, o[0])
-
 
 wire4 = Wire(
     [Line([m0, top[0]]), Spline([top[0], Vector(0, 19.25, -23.7), m1]), Line(m1, m0)]
 )
 
-wire5 = Wire(
-    [
-        Spline([m6, Vector(8.2, 0, -22.9), top[1]]),
-        Spline([top[1], Vector(12, 25, -27.6), m2]),
-        Line(m2, m6),
-    ]
-)
 
 wire6 = Wire(
     [
-        Spline([o[1], top[2]]),
+        Line([o[1], top[2]]),
         Spline([top[2], Vector(24.8, 18.8, -34.6), o[0]]),
         Line(o[0], o[1]),
     ]
 )
 
+wire8 = Wire(Polyline([o[1], top[2]], top[1], m6, o[1]))
+wire9 = Wire(Polyline([m6, top[1], top[0], m0, m6]))
+wire10 = Wire([Spline([o[1], m5, d5, b5]), Line(b5, b6), Line(b6, m6), Line(m6, o[1])])
+wire11 = Wire(Polyline(b0, m0, m6, b6, b0))
+wire12 = Wire(Polyline(b1, m1, m2, b2, b1))
+wire13 = Wire(Polyline(b0, b1, b2, b6, b0))
+wire14 = Wire(Polyline(b6, b2, b3, b5, b6))
+wire15 = Wire([Line(b2, m2), Line(m2, o[0]), Spline([b3, d3, m3, o[0]]), Line(b3, b2)])
+wire16 = Wire(
+    [
+        Line(top[0], top[1]),
+        Spline([top[1], Vector(12, 25, -27.6), m2]),
+        Line(m2, m1),
+        Spline([top[0], Vector(0, 19.25, -23.7), m1]),
+    ]
+)
+wire17 = Wire(
+    [
+        Line(top[2], top[1]),
+        Spline([top[1], Vector(12, 25, -27.6), m2]),
+        Line(m2, o[0]),
+        Spline([top[2], Vector(24.8, 18.8, -34.6), o[0]]),
+    ]
+)
 
-p5 = Polyline(top)
-p6 = p3
-p7 = p4
+solid = Solid(
+    Shell(
+        [
+            Face.make_surface(wire)
+            for wire in [
+                wire1,
+                wire3,
+                wire4,
+                wire6,
+                wire8,
+                wire9,
+                wire10,
+                wire11,
+                wire12,
+                wire13,
+                wire14,
+                wire15,
+                wire16,
+                wire17,
+            ]
+        ]
+    )
+)
 
-# f = sweep(sections=[wire4,wire5],path=p5,multisection=True)
 
 if __name__ == "__main__":
     set_port(3939)
